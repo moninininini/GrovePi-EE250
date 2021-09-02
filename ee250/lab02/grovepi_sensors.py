11feb3a3ae5c9d1bf67db38d7a567a5ef0b27f7d
Monica Li <xinmingl@usc.edu>
2021-09-01 17:24:44 -0700

    try

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,11 +1,12 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
 List team members here.
+Monica Li
 
 Insert Github repository link here.
-"""
+"https://github.com/moninininini/GrovePi-EE250"
 
-"""python3 interpreters in Ubuntu (and other linux distros) will look in a 
+"python3 interpreters in Ubuntu (and other linux distros) will look in a 
 default set of directories for modules when a program tries to `import` one. 
 Examples of some default directories are (but not limited to):
   /usr/lib/python3.5
@@ -23,16 +24,30 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
+grovepi.set_bus("RPI_1")
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
+
+# Connect the Grove Rotary Angle Sensor to analog port A0
+potentiometer = 0
+
 if __name__ == '__main__':
     PORT = 4    # D4
-
+    setText("Hello world\nThis is an LCD test")
+    setRGB(0,128,64)
+    time.sleep(2)
     while True:
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
-
-        print(grovepi.ultrasonicRead(PORT))
+      time.sleep(0.2)
+      print("hi")
+      #So we do not poll the sensors too quickly which may introduce noise,
+      # sleep for a reasonable time of 200ms between each iteration.
+      # range = grovepi.ultrasonicRead(PORT)
+      # setText(str(range))
+      # threshold = grovepi.analogRead(potentiometer)
+      # print("threshold is: ", threshold)
+      # print("range: ",range)
+    
+      # sensor_value = grovepi.analogRead(potentiometer)
