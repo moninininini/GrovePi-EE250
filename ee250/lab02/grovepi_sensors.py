a580ea77e1056eb58e3576802959fa84cd4f5cbe
Monica Li <xinmingl@usc.edu>
2021-09-04 21:51:02 -0700

    try

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -47,9 +47,10 @@ if __name__ == '__main__':
       print("hi Enji Love you")
       range = grovepi.ultrasonicRead(PORT)
       threshold = grovepi.analogRead(potentiometer)
-      setText_norefresh(" "+str(threshold)+"cm           \n "+str(range)+"cm")
       
       if range < threshold:
         setText_norefresh(" "+str(threshold)+"cm OBJ PRES\n "+str(range)+"cm")
+      else:
+        setText_norefresh(" "+str(threshold)+"cm           \n "+str(range)+"cm")
 
 
